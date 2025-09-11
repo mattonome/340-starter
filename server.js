442353dba06bf452d0ca2c3e80780e04ac12ccfe
mattonome <mattj4all@yahoo.com>
2025-09-11 11:33:26 +0100

    now

--- server.js
+++ server.js
@@ -1,50 +1,36 @@
-/* ******************************************
- * This server.js file is the primary file of the 
- * application. It is used to control the project.
- *******************************************/
-/* ***********************
- * Require Statements
- *************************/
+/********************************************
+ * Main server.js file
+ ********************************************/
 const express = require("express")
 const env = require("dotenv").config()
-const app = express()
-const static = require("./routes/static")
 const expressLayouts = require("express-ejs-layouts")
+
+const app = express()
 
 /* ***********************
  * View Engine and Templates
  *************************/
-
-
 app.set("view engine", "ejs")
 app.use(expressLayouts)
-app.set("layout", "./layouts/layout") //not at views port
+app.set("layout", "./layouts/layout") // not at views root
 
 /* ***********************
- * Routes
+ * Static Files
  *************************/
-app.use(static)
-
+app.use(express.static("public"))
 
 /* ***********************
  * Index Route
  *************************/
-app.get("/", function (req, res) {
-  res.render("index", {title: "Home"})
+app.get("/", (req, res) => {
+  res.render("index", { title: "Home" })
 })
 
-
-
 /* ***********************
- * Local Server Information
- * Values from .env (environment) file
+ * Server Setup
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT || 3000
 
-/* ***********************
- * Log statement to confirm server operation
- *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
+  console.log(`App running on port ${port}`)
 })
